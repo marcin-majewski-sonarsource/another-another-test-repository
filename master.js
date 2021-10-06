c43602a28bef59a7c34cc6fec0cebd78548895f2
Marcin Majewski <marcin.majewski@sonarsource.com>
2021-10-06 10:52:15 +0200

    Move out of test

---
+++ master.js
@@ -0,0 +1,15 @@
+let nonCallable = 1;
+let other = 2;
+
+function withTooManyParams(a, b, c, d, e) {
+    return a + b + c + d + e;
+}
+
+;;;;
+;;;;;;;;;;;;;;
+
+nonCallable();
+
+var myWindow = document.getElementById('myIFrame').contentWindow;
+myWindow.postMessage(message, "*");
+print 'test'
